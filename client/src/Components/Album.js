bdb20585db9be21394af6a463911f3ba6300a5b3
Mikan <104711459+MikanW@users.noreply.github.com>
2024-03-28 01:10:12 +0900

    kudo button onclick: color change & Photo component

--- client/src/Components/Album.js
+++ client/src/Components/Album.js
@@ -6,10 +6,13 @@ import ImageListItemBar from '@mui/material/ImageListItemBar';
 import IconButton from '@mui/material/IconButton';
 import FavoriteIcon from '@mui/icons-material/Favorite';
 import axios from 'axios';
+import Photo from './Photo';
 
 
 function Album() {
   const [apiResponse, setApiResponse] = useState([]);
+  const [kudoStatus, setKudoStatus] = useState(false);
+  const [kudoColor, setKudoColor] = useState('rgba(166, 24, 24, 0.143)');
 
   useEffect(() => {
     const callAPI = async () => {
@@ -21,7 +24,13 @@ function Album() {
     callAPI();
   }, []);
 
-  console.log(apiResponse);
+  const onKudoClick = () => {
+    console.log("kudos!")
+    if (false == kudoStatus) {
+      setKudoStatus(true);
+      setKudoColor('rgba(177, 36, 36, 0.734)');
+    }
+  }
 
   return (
     <div className='WrapAlbum'>
@@ -29,29 +38,35 @@ function Album() {
       <Box className='Album'>
         <ImageList variant="masonry" cols={3} gap={8}>
           {apiResponse.map((item) => (
-            <ImageListItem key={item._id} className='PhotoCard'>
-              <img
-                className='Photo'
-                srcSet={`${item.url}?w=248&fit=crop&auto=format&dpr=2 2x`}
-                src={`${item.url}?w=248&fit=crop&auto=format`}
-                alt={item.tags + ", " + item.location + ", "}
-                loading="lazy"
-              />
-              <ImageListItemBar
-                position="below"
-                title={item.tags + " " + item.location}
-                actionIcon={
-                  <IconButton className='KudoButton'
-                    sx={{ color: 'rgba(166, 24, 24, 0.143)' }}
-                    aria-label={`Left Kudos for ${item.title}`}
-                  >
-                    <p>{item.kudos}</p>
-                    <FavoriteIcon />
-                  </IconButton>
-                }
-              />
-            </ImageListItem>
+            // <ImageListItem key={item._id} className='PhotoCard'>
+            //   <img
+            //     className='Photo'
+            //     srcSet={`${item.url}?w=248&fit=crop&auto=format&dpr=2 2x`}
+            //     src={`${item.url}?w=248&fit=crop&auto=format`}
+            //     alt={item.tags + ", " + item.location + ", "}
+            //     loading="lazy"
+            //   />
+            //   <ImageListItemBar
+            //     position="below"
+            //     title={item.tags + " " + item.location}
+            //     actionIcon={
+            //       <IconButton className='KudoButton'
+            //         sx={{ color: kudoColor }}
+            //         aria-label={`Left Kudos for ${item.title}`}
+            //         onClick={onKudoClick}
+            //       >
+            //         <FavoriteIcon />
+            //         <p>{item.kudos}</p>
+            //       </IconButton>
+            //     }
+            //   />
+            // </ImageListItem>
+            <Photo
+              key={item._id}
+              item={item}
+            />
           ))}
+
         </ImageList>
       </Box>
     </div>
